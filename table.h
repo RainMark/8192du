59591703328eed2f87a711260e27bf12e4baacb9
Bingyu Zhou <rain.by.zhou@gmail.com>
2016-10-25 17:04:54 +0800

    Add table.c from hal8192duhwimg.c and changed table.h

--- table.h
+++ table.h
@@ -28,30 +28,30 @@
  * Created on  2010/ 5/18,  1:41
  *****************************************************************************/
 
-#ifndef __RTL92DE_TABLE__H_
-#define __RTL92DE_TABLE__H_
+#ifndef __RTL92DU_TABLE__H_
+#define __RTL92DU_TABLE__H_
 
 /*Created on  2011/ 1/14,  1:35*/
 
-#define PHY_REG_2T_ARRAYLENGTH 380
-extern u32 rtl8192de_phy_reg_2tarray[PHY_REG_2T_ARRAYLENGTH];
+#define PHY_REG_2T_ARRAYLENGTH 372
+extern u32 rtl8192du_phy_reg_2tarray[PHY_REG_2T_ARRAYLENGTH];
 #define PHY_REG_ARRAY_PG_LENGTH 624
-extern u32 rtl8192de_phy_reg_array_pg[PHY_REG_ARRAY_PG_LENGTH];
+extern u32 rtl8192du_phy_reg_array_pg[PHY_REG_ARRAY_PG_LENGTH];
 #define RADIOA_2T_ARRAYLENGTH 378
-extern u32 rtl8192de_radioa_2tarray[RADIOA_2T_ARRAYLENGTH];
+extern u32 rtl8192du_radioa_2tarray[RADIOA_2T_ARRAYLENGTH];
 #define RADIOB_2T_ARRAYLENGTH 384
-extern u32 rtl8192de_radiob_2tarray[RADIOB_2T_ARRAYLENGTH];
+extern u32 rtl8192du_radiob_2tarray[RADIOB_2T_ARRAYLENGTH];
 #define RADIOA_2T_INT_PA_ARRAYLENGTH 378
-extern u32 rtl8192de_radioa_2t_int_paarray[RADIOA_2T_INT_PA_ARRAYLENGTH];
+extern u32 rtl8192du_radioa_2t_int_paarray[RADIOA_2T_INT_PA_ARRAYLENGTH];
 #define RADIOB_2T_INT_PA_ARRAYLENGTH 384
-extern u32 rtl8192de_radiob_2t_int_paarray[RADIOB_2T_INT_PA_ARRAYLENGTH];
-#define MAC_2T_ARRAYLENGTH 160
-extern u32 rtl8192de_mac_2tarray[MAC_2T_ARRAYLENGTH];
+extern u32 rtl8192du_radiob_2t_int_paarray[RADIOB_2T_INT_PA_ARRAYLENGTH];
+#define MAC_2T_ARRAYLENGTH 192
+extern u32 rtl8192du_mac_2tarray[MAC_2T_ARRAYLENGTH];
 #define AGCTAB_ARRAYLENGTH 386
-extern u32 rtl8192de_agctab_array[AGCTAB_ARRAYLENGTH];
+extern u32 rtl8192du_agctab_array[AGCTAB_ARRAYLENGTH];
 #define AGCTAB_5G_ARRAYLENGTH 194
-extern u32 rtl8192de_agctab_5garray[AGCTAB_5G_ARRAYLENGTH];
+extern u32 rtl8192du_agctab_5garray[AGCTAB_5G_ARRAYLENGTH];
 #define AGCTAB_2G_ARRAYLENGTH 194
-extern u32 rtl8192de_agctab_2garray[AGCTAB_2G_ARRAYLENGTH];
+extern u32 rtl8192du_agctab_2garray[AGCTAB_2G_ARRAYLENGTH];
 
 #endif
